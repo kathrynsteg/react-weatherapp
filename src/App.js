08a61dc8c0b532d2fbe0ed6d00b332f79da66473
kathrynsteg <kathrynsteg@gmail.com>
2024-07-05 23:48:37 -0500

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -2,24 +2,26 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div>
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://kathryn-stegeman-portfolio.netlify.app/"
-          target="_blank"
-        >
-          Kathryn Stegeman
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/kathrynsteg/react-weatherapp"
-          target="_blank"
-        >
-          open-sourced on GitHub.
-        </a>
-      </footer>
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://kathryn-stegeman-portfolio.netlify.app/"
+            target="_blank"
+          >
+            Kathryn Stegeman
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/kathrynsteg/react-weatherapp"
+            target="_blank"
+          >
+            open-sourced on GitHub.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
