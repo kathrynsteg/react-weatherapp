83199f4c957339f5d33d97ea5280bafdda6e629f
kathrynsteg <kathrynsteg@gmail.com>
2024-07-05 23:39:14 -0500

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -4,6 +4,22 @@ export default function App() {
   return (
     <div>
       <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://kathryn-stegeman-portfolio.netlify.app/"
+          target="_blank"
+        >
+          Kathryn Stegeman
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/kathrynsteg/react-weatherapp"
+          target="_blank"
+        >
+          open-sourced on GitHub.
+        </a>
+      </footer>
     </div>
   );
 }
